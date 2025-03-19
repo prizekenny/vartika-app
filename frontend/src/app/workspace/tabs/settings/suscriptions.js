f0bda64df4bfdd9417b8c2dbb9a976eaa09c2990
Kenny <prizekenny@gmail.com>
2025-03-19 13:34:33 -0600

    Add authorize page in settings tab

---
+++ frontend/src/app/workspace/tabs/settings/suscriptions.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SubscriptionsTab = () => {
+  return (
+    <div>
+      <h3 className="text-xl font-bold mb-6">Subscriptions</h3>
+      <p className="text-gray-500">
+        Subscription management will be available here.
+      </p>
+    </div>
+  );
+};
+
+export default SubscriptionsTab;
